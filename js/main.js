eaeb1373b535c165dc471f466eaee942340c02bd
thomasrouiller <thomas.rouiller@studentfr.ch>
2022-09-12 15:53:57 +0200

    let's go

--- js/main.js
+++ js/main.js
@@ -2,6 +2,8 @@ var yellow = document.querySelector(".change-color");
 var orange = document.querySelector(".toggle-color");
 var pink = document.querySelector(".trigger");
 var red = document.querySelector(".target");
+var green = document.querySelector(".trigger-all");
+var turquoise = document.querySelector(".trigger-all-self");
 
 var changeColorToBlue = function () {
   yellow.classList.add("is-active");
@@ -19,7 +21,20 @@ var changeOpacityPink = function () {
   pink.classList.add("is-active");
 };
 
+var changeBoxesColorBlueRed = function () {
+  yellow.classList.add("is-active");
+  orange.classList.add("is-active");
+};
+
+var changeBoxesColorAndSelf = function () {
+  yellow.classList.add("is-active");
+  orange.classList.add("is-active");
+  turquoise.classList.add("is-active");
+};
+
 yellow.addEventListener("click", changeColorToBlue);
 orange.addEventListener("click", changeColorToRed);
 pink.addEventListener("click", changeOpacityRed);
 red.addEventListener("click", changeOpacityPink);
+green.addEventListener("click", changeBoxesColorBlueRed);
+turquoise.addEventListener("click", changeBoxesColorAndSelf);
